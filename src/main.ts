cca78599758406754d357f0f03148db0dc7d290b
Yoga Darmawan <the.yogadarmawan@gmail.com>
2020-09-17 17:08:52 +0700

    feat: set up styles and normalize css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// NORMALIZE CSS, RESET BROWSER DEFAULT CSS
+import 'normalize.css';
+// GLOBAL CSS NIT
+import '@/assets/styles/index.scss';
 
 Vue.config.productionTip = false;
 
